a0cea77095a48230ce0652999603c5510cc4b9b8
Kumar <70212520+nexus1203@users.noreply.github.com>
2023-03-25 14:12:29 +0800

    Add files via upload

--- pyroboklit/tcp_transport/__init__.py
+++ pyroboklit/tcp_transport/__init__.py
@@ -1,1 +1,2 @@
-
+from .api_port import *
+from .transport import *
---
+++ pyroboklit/tcp_transport/api_port.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+╔═══════════════════════════════════════════════════════════════╗
+║ File:           api_port.py                                   ║
+║ Author:         Nexus1203                                     ║
+║ Created:        2023-03-23                                    ║
+║ Last Modified:  2023-03-23                                    ║
+║ Description:    TCP API PORTS for different communications.   ║  
+╚═══════════════════════════════════════════════════════════════╝
+"""
+
+API_PORT_ROBOT = 19200
+API_PORT_STATE = 19204
+API_PORT_CTRL = 19205
+API_PORT_TASK = 19206
+API_PORT_CONFIG = 19207
+API_PORT_KERNEL = 19208
+API_PORT_OTHER = 19210
+API_PUSH_PORT = 19301
