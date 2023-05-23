237797c9cdd3e9ef6004aed167cd51ded4999a6d
Linux *User <70212520+nexus1203@users.noreply.github.com>
2023-05-23 18:11:02 +0800

    Update test_pushnotification.py

--- test_pushnotification.py
+++ test_pushnotification.py
@@ -1,14 +1,15 @@
-from pyrobokit.agv_api import Notification
+from pyrobokit.agv_api import push_notification
 from time import sleep
 from time import perf_counter as pf
 
-ip = "192.168.11.191"
-notify = Notification(ip)
+ip = "192.168.0.10"
+notify = push_notification.Notification(ip)
 
-notify.configure_monitoring(interval=500)
+notify.configure_monitoring(interval=1000) # keep it above 500 ms for stable response
 
 for i in range(1000):
     start = pf()
     data = notify.receive()
     print(f"interval:: {pf()-start}")
     print(data)
+    sleep(1)
